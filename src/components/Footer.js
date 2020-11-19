524aa087e5d576273d1f76d52b623dd3c7c42042
August <hachigatsu14@outlook.com>
2020-11-19 10:10:05 +0000

    updated linkedin link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
         <>
             <footer>
                 <p>Designed with <Emoji symbol="🖤" label="love" /> by August</p>
-                <p><a href="https://linkedin.com/in/august-a-66894784"><FaLinkedin aria-label="Visit my LinkedIn profile" /></a></p>
+                <p><a href="https://www.linkedin.com/in/%E2%9C%A8-august-a-66894784/"><FaLinkedin aria-label="Visit my LinkedIn profile" /></a></p>
                 <p><a href="https://github.com/Augs0"><FaGithubSquare aria-label="Visit my Github profile" /></a></p>
                 <p><a href="https://codepen.io/augs0"><FaCodepen aria-label="Visit my Codepen profile" /></a></p>
             </footer>
